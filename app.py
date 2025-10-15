bfd73e9f72a46253a69868938274af583904a032
rishu <utkarshsinghmz@gmail.com>
2025-10-15 20:34:25 +0530

    fist commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With Ollama"
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful massistant . Please  repsonse to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+def generate_response(question,llm,temperature,max_tokens):
+    llm=Ollama(model=llm)
+    output_parser=StrOutputParser()
+    chain=prompt|llm|output_parser
+    answer=chain.invoke({'question':question})
+    return answer
+
+## #Title of the app
+st.title("Simple Q&A Chatbot With Ollama")
+ 
+
+## Select the Ollama model
+llm=st.sidebar.selectbox("Select Open Source model",["mistral"])
+
+## Adjust response parameter
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.65)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=100)
+
+## Main interface for user input
+st.write("What can I help with?")
+user_input=st.text_input("You:")
+
+
+
+if user_input :
+    response=generate_response(user_input,llm,temperature,max_tokens)
+    st.write(response)
+else:
+    st.write("Ask anything")
+
+
